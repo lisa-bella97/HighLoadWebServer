e804ccbf2b564d22c5496de32f26bc7fe64aa9af
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2020-03-03 01:56:09 +0300

    Temp version of server

---
+++ source/Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liza on 02.03.20.
+//
+
+#include "Server.h"
--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <ServerConfigParser.h>
+#include <Server.h>
 
 int main() {
     auto server_config = ServerConfigParser::parse("../httpd.conf");
     std::cout << "document_root = " << server_config.document_root << std::endl <<
               "cpu_limit = " << server_config.cpu_limit << std::endl;
+
+    try
+    {
+        boost::asio::io_service io_service;
+
+        server s(io_service, 80);
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
     return 0;
 }
